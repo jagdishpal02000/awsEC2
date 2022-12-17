4a6e94726cdee78b4baefb7fc349b38cfdee7e36
Jagdish Pal <Jagdish Pal>
2022-12-17 08:50:34 +0530

    message changed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 5000;
 app.get('/', (req, res) =>{
-    res.json({message:'Hii api from hosted in ec2'});
+    res.json({message:'Hii, i\'m in ec2'});
 })
 app.listen(port,()=>{
     console.log('SERVER IS LISTENING ON PORT ',port);
